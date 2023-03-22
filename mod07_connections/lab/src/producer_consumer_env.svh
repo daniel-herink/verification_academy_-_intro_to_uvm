4d6516653345fc003b28a3ccff09a7212a9a9728
Daniel Herink <work+github@danielherink.com>
2023-03-22 16:10:18 -0700

    Finished lab 7

--- mod07_connections/lab/src/producer_consumer_env.svh
+++ mod07_connections/lab/src/producer_consumer_env.svh
@@ -6,10 +6,11 @@ class producer_consumer_env extends uvm_env;
    consumer c;
 
    // Please declare a variable to hold the square_it object
-   
+   square_it env_square_it;
    uvm_tlm_fifo #(int)    producer2square_it;
 
    // Please declare a uvm_tlm_fifo #(int) that will go from square_it to the consumer.
+   uvm_tlm_fifo #(int) square_it2consumer;
    
    function new(string name, 
                 uvm_component parent);
@@ -21,10 +22,10 @@ class producer_consumer_env extends uvm_env;
       p = producer::type_id::create("p",this);
       c = consumer::type_id::create("c",this);
       // Please use the factory to get a new square_it object
-
+      env_square_it = square_it::type_id::create("square_it", this);
       producer2square_it = new ("producer2square_it",this);
       // Please create a new uvm_tlm_fifo to go from the square_it object to the consumer
-
+      square_it2consumer = new("square_it2consumer", this);
    endfunction: build_phase
 
    function void connect_phase(uvm_phase phase);
@@ -33,10 +34,11 @@ class producer_consumer_env extends uvm_env;
       
       // Please connect your square_it object to the producer2square_it uvm_tlm_fifo
       //  remember to use the get_export object in the fifo.
-      // 
+      env_square_it.get_func.connect(producer2square_it.get_export);
       // Please connect your square_it object to the uvm_tlm_fifo that goes from the 
       // square_it object to the consumer.  You declared this uvm_tlm_fifo above.
       // Remember to use the put_export from your uvm_tlm_fifo.
+      env_square_it.put_func.connect(square_it2consumer.put_export);
 
       c.phone2.connect(square_it2consumer.get_export);  
       
--- mod07_connections/lab/src/square_it.svh
+++ mod07_connections/lab/src/square_it.svh
@@ -3,8 +3,9 @@ class square_it extends uvm_agent;
   `uvm_component_utils(square_it)    
   
    // Please declare an "uvm_get_port #(int)" that will get data from the producer.
+  uvm_get_port #(int) get_func;
    // Please declare an "uvm_put_port #(int)" that will put data into the consumer.
-
+  uvm_put_port #(int) put_func;
   // constructor
    
   function new(string name, uvm_component parent);
@@ -16,8 +17,10 @@ class square_it extends uvm_agent;
     super.run_phase(phase);
     forever begin
        // Please get an integer out of the uvm_get_port
+      get_func.get(i);
        // Please put the square of the integer in the uvm_put_port
        // The square operator in verilog is ** as in "3**2 is 9"
+      put_func.put(i**2);
        
 
     end
@@ -28,8 +31,9 @@ class square_it extends uvm_agent;
   function void build_phase(uvm_phase phase);
   super.build(phase);
      // Please create a new uvm_get_port using the variable you declared above
+    get_func = new("output", this);
      // Please create a new uvm_put_port using the variable you declared above
-     
+    put_func = new("input", this);
 
 endfunction : build
 
